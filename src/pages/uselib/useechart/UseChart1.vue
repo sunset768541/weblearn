78a4cafb2c692c58c0711d2f1b23c4989cde5f73
wangbaohui <13802885224@139.com>
2023-02-13 17:30:37 +0800

    显示第一个图表

--- src/pages/uselib/useechart/UseChart1.vue
+++ src/pages/uselib/useechart/UseChart1.vue
@@ -1,12 +1,45 @@
 <template>
-  <div>
-    1
+  <div id="mainee" v-show="true" style="width: 600px; height: 400px">
+    <div id="ssss" style="width: 600px; height: 400px"></div>
   </div>
 </template>
 
 <script>
+import * as echarts from 'echarts'
 export default {
+    mounted() {
+      console.log("是是是",document.getElementById('ssss'))
+      // 基于准备好的dom，初始化echarts实例
+      let myChart = echarts.init(document.getElementById('ssss'));
+      // 指定图表的配置项和数据
+      var option = {
+        title: {
+          text: 'ECharts 入门示例'
+        },
+        tooltip: {},
+        legend: {
+          data: ['销量']
+        },
+        xAxis: {
+          data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+        },
+        yAxis: {},
+        series: [
+          {
+            name: '销量',
+            type: 'bar',
+            data: [5, 20, 36, 10, 10, 20]
+          }
+        ]
+      };
 
+      // 使用刚指定的配置项和数据显示图表。
+      myChart.setOption(option);
+    },
+created() {
+  this.$nextTick(function () {
+  })
+},
 }
 </script>
 
